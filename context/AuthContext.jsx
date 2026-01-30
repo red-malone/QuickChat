546b20ebc0ef8fd9fcbec9d8e23a64e298b3037c
rohi-ps <rohit.psudarsanan@gmail.com>
2026-01-30 21:21:07 +0530

    authcontext

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -1,11 +1,28 @@
+
+import axios from "axios";
 import { createContext } from "react";
 
 const BACKEND_URL=import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL=BACKEND_URL;
 export const AuthContext = createContext();
 
 export const AuthProvider=({children})=>{
+    const [token,setToken]=useState(localStorage.getItem("token"));
+    const [authUser,setAuthUser]=useState(null);
+    const [onlineUsers,setOnlineUsers]=useState([]);
+    const [socket,setSocket]=useState(null);
+    const checkAuth=async()=>{
+        try{
+            const {data}=await axios.get("/")
+        }catch(error){
+            console.error("Error checking authentication:", error.message);
+        }
+    }
     const value={
-
+        axios,
+        authUser,
+        onlineUsers,
+        socket
     }
 
     return(
